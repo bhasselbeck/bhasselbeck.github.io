71745b2ac35d960e8ceaaae9bf58daafeba31715
brianhasselbeck <hasselbeck.brian@gmail.com>
2015-08-24 18:51:25 -0400

    New code and template

--- assets/js/social.js
+++ assets/js/social.js
@@ -1,5 +1,5 @@
 var SocialMedia = function() {
-  this.apiUrl = "http://eebhasselbeckdev.prod.acquia-sites.com/v1";
+  this.apiUrl = "http://api.bhasselbeck.local/v1";
 };
 
 SocialMedia.prototype.request = function(url, callback) {
@@ -21,62 +21,21 @@ SocialMedia.prototype.getSocial = function(platform, callback) {
   });
 };
 
-var socialContent = function(post) {
-  var output = '';
-  var context = {
-    link: post.link,
-    uri: post.image_uri,
-    message: post.message.substr(0, 28),
-    action: 'Check it out'
-  }
-  output += Handlebars.templates.social(context);
-  return output;
-}
-
 var social = function () {
   return {
     init: function () {
       var request = new SocialMedia();
       request.getSocial('facebook', function(data) {
-        var output = '';
+        var context = {}
         $.each(data, function(key, post) {
-          if (key < 4) {
-            if (key == 0) {
-              output += '<div class="row row-space-10">';
-            }
-            output += socialContent(post);
-            if (key == 3) {
-              output += '</div>';
-            }
-          }
-          if (key >= 4 && key < 8) {
-            if (key == 4) {
-              output += '<div class="row row-space-10">';
-            }
-            output += socialContent(post);
-            if (key == 7) {
-              output += '</div>';
-            }
-          }
-          if (key >= 8 && key < 12) {
-            if (key == 8) {
-              output += '<div class="row row-space-10">';
-            }
-            output += socialContent(post);
-            if (key == 11) {
-              output += '</div>';
-            }
-          }
-          if (key >= 12 && key < 16) {
-            if (key == 12) {
-              output += '<div class="row row-space-10">';
-            }
-            output += socialContent(post);
-            if (key == 15) {
-              output += '</div>';
-            }
+          context[key] = {
+            link: post.link,
+            uri: post.image_uri,
+            message: post.message.substr(0, 28),
+            action: 'Check it out'
           }
         });
+        var output = Handlebars.templates.social(context);
         $("#life .container").append(output);
       });
     }
